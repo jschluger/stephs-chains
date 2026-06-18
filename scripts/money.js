ade45583e0e769e5dcc596aff9b13133f59ad9e4
Charlotte <charlotteschluger@gmail.com>
2026-06-17 20:34:41 -0400

    q

--- scripts/money.js
+++ scripts/money.js
@@ -1,9 +1,3 @@
-var onC = function() {
-    var r = Math.floor(Math.random() * 100000);
-    $('body').append('<img id="money'+r+'" src="assets/100.jpeg" alt="money"> </img>');
-    var im = $('#money'+r);
-    im.css('position','fixed');
-    im.css('top',$('#stephmoney').css('top') + Math.floor(Math.random() * 50)-25);
-    im.css('left',$('#stephmoney').css('left') + Math.floor(Math.random() * 50)-25);
+if (window.innerWidth > window.innerHeight) {
+    $('#entrypic').css("rotate", "90deg")
 }
-$('#stephmoney').click(onC);
